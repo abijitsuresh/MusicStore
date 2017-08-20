5a17c4b868db740a16372093367b5fcbbbfd5c08
Abijit Suresh Geetha Ram <abijit.suresh@outlook.com>
2017-08-20 03:41:35 -0500

    Initial commit.

---
+++ MusicStore/Migrations/201708200746521_InitialCreate.cs
@@ -0,0 +1,26 @@
+namespace MusicStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Artists",
+                c => new
+                    {
+                        ArtistID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.ArtistID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Artists");
+        }
+    }
+}
